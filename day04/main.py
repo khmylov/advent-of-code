1f85a08573f3fd36a675f998899ae0c2f6a196b6
Andrew Khmylov <andrew.khmylov@gmail.com>
2021-02-01 16:59:55 +0300

    Add day04

--- day04/main.py
+++ day04/main.py
@@ -1,3 +1,5 @@
+import re
+
 def read_input():
     result, current_item = [], {}
     for line in open('input.txt').read().splitlines():
@@ -22,6 +24,31 @@ def is_valid(item):
             and 'ecl' in item
             and 'pid' in item)
 
+def is_valid2(item):
+    if not is_valid(item): return False
+
+    byr = int(item['byr'])
+    if byr < 1920 or byr > 2002: return False
+    iyr = int(item['iyr'])
+    if iyr < 2010 or iyr > 2020: return False
+    eyr = int(item['eyr'])
+    if eyr < 2020 or eyr > 2030: return False
+    hgt_text = item['hgt']
+    if hgt_text.endswith("cm"):
+        hgt = int(hgt_text[:-2])
+        if hgt < 150 or hgt > 193: return False
+    elif hgt_text.endswith("in"):
+        hgt = int(hgt_text[:-2])
+        if hgt < 59 or hgt > 76: return False
+    else: return False
+    if re.compile(r'^#[0-9a-f]{6}$').match(item['hcl']) is None: return False
+    if re.compile(r'^(amb|blu|brn|gry|grn|hzl|oth)$').match(item['ecl']) is None: return False
+    if re.compile(r'^[0-9]{9}$').match(item['pid']) is None: return False
+    return True
+
 
 # part1 - 245
 print(len([x for x in read_input() if is_valid(x)]))
+
+# part2 - 133
+print(len([x for x in read_input() if is_valid2(x)]))
