ab21a49fc415857f1288bfaefd991c6f6e3992bb
Andrew Khmylov <andrew.khmylov@gmail.com>
2021-12-07 21:21:24 +0300

    Add day06

---
+++ 2021/day06/main.fsx
@@ -0,0 +1,19 @@
+// let inputText = "3,4,3,1,2"
+let inputText = "1,1,1,2,1,5,1,1,2,1,4,1,4,1,1,1,1,1,1,4,1,1,1,1,4,1,1,5,1,3,1,2,1,1,1,2,1,1,1,4,1,1,3,1,5,1,1,1,1,3,5,5,2,1,1,1,2,1,1,1,1,1,1,1,1,5,4,1,1,1,1,1,3,1,1,2,4,4,1,1,1,1,1,1,3,1,1,1,1,5,1,3,1,5,1,2,1,1,5,1,1,1,5,3,3,1,4,1,3,1,3,1,1,1,1,3,1,4,1,1,1,1,1,2,1,1,1,4,2,1,1,5,1,1,1,2,1,1,1,1,1,1,1,1,2,1,1,1,1,1,5,1,1,1,1,3,1,1,1,1,1,3,4,1,2,1,3,2,1,1,2,1,1,1,1,4,1,1,1,1,4,1,1,1,1,1,2,1,1,4,1,1,1,5,3,2,2,1,1,3,1,5,1,5,1,1,1,1,1,5,1,4,1,2,1,1,1,1,2,1,3,1,1,1,1,1,1,2,1,1,1,3,1,4,3,1,4,1,3,2,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,2,1,5,1,1,1,1,2,1,1,1,3,5,1,1,1,1,5,1,1,2,1,2,4,2,2,1,1,1,5,2,1,1,5,1,1,1,1,5,1,1,1,2,1"
+let input = inputText.Split(',') |> Seq.map (System.Int32.Parse) |> List.ofSeq
+
+let step =
+  List.collect (fun (timer, count) -> if timer = 0 then [6, count; 8, count] else [timer - 1, count])
+  >> List.groupBy fst
+  >> List.map (fun (timer, group) -> timer, List.sumBy snd group)
+
+let run days =
+  let initMap = input |> Seq.countBy id |> Seq.map (fun (timer, count) -> timer, bigint count) |> List.ofSeq
+  seq {1..days}
+  |> Seq.fold (fun map _ -> step map) initMap
+  |> List.sumBy snd
+
+// 391671
+let part1 = run 80
+// 1754000560399
+let part2 = run 256
